58a07c2a2a1de1fb2751c430799e37b77c478477
alice <bow1226@gmail.com>
2020-06-20 18:28:51 +0800

    Revert "chore: Update requirement.txt"
    This reverts commit 120cd25901fe0eff691a35bc9472fe2874b423d7.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
 source venv/bin/activate
 pip3 install -r requirements.txt
 mkdir models
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-0001 -o models/
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name head-pose-estimation-adas-0001 -o models/
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name facial-landmarks-35-adas-0002 -o models/
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name gaze-estimation-adas-0002 -o models/
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-retail-0004 -o models/ --precisions FP16
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name head-pose-estimation-adas-0001 -o models/ --precisions FP16
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name facial-landmarks-35-adas-0002 -o models/ --precisions FP16
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name gaze-estimation-adas-0002 -o models/ --precisions FP16
