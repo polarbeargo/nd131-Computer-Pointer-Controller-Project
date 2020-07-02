1b492ad64cd8dbbe3b2adcb18d98731501fec8b8
Hsin-Wen Chang <bow1226@gmail.com>
2020-07-02 10:40:17 +0800

    chore: Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source venv/bin/activate
 source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6 
 pip3 install -r requirements.txt
 mkdir models
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-0001 -o models/
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name face-detection-adas-binary-0001 -o models/
 python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name head-pose-estimation-adas-0001 -o models/
-python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name facial-landmarks-35-adas-0002 -o models/
+python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name landmarks-regression-retail-0009 -o models/
 python3 /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py --name gaze-estimation-adas-0002 -o models/
